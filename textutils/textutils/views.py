5ea52e35e6df18ccbdec99ee7b5164f55bf4356f
abhinavjha98 <abhinavjha98ald@gmail.com>
2020-11-21 10:06:55 +0530

    Added the bootstrap and added switches

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -5,7 +5,66 @@ def index(request):
 	return render(request,'index.html')
 
 def analyze(request):
-	return render(request,'index.html')
+	djtext = request.POST.get('text',"default")
+	removepunc = request.POST.get('removepunc',"off")
+	fullcaps = request.POST.get('fullcaps', 'off')
+	newlineremover = request.POST.get('newlineremover', 'off')
+	extraspaceremover = request.POST.get('extraspaceremover', 'off')
+	if removepunc == "on":
+		analyzed = ""
+		punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+		for char in djtext:
+			if char not in punctuations:
+				analyzed = analyzed + char
+		params ={
+			'purpose':'removed punctuation',
+			'analyzed_text': analyzed
+		}
+		djtext=analyzed
+		# return render(request,'analyze.html',params)
+
+	if(fullcaps=="on"):
+		analyzed = ""
+		for char in djtext:
+			analyzed = analyzed + char.upper()
+		params = {
+			'purpose': 'Changed to Uppercase', 
+			'analyzed_text': analyzed
+			}
+		djtext=analyzed
+        # Analyze the text
+		# return render(request, 'analyze.html', params)
+		
+	if(extraspaceremover=="on"):
+		analyzed = ""
+		for index, char in enumerate(djtext):
+			if not(djtext[index] == " " and djtext[index+1]==" "):
+				analyzed = analyzed + char
+		params = {
+			'purpose': 'Removed NewLines', 
+			'analyzed_text': analyzed
+			}
+		djtext=analyzed
+        # Analyze the text
+		# return render(request, 'analyze.html', params)
+	
+	if (newlineremover == "on"):
+		analyzed = ""
+		for char in djtext:
+			if char != "\n" and char!="\r":
+				analyzed = analyzed + char
+			else:
+				print("no")
+		print("pre", analyzed)
+		params = {
+			'purpose': 'Removed NewLines', 
+			'analyzed_text': analyzed}
+        # Analyze the text
+		
+	
+	if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on"):
+		return HttpResponse("please select any operation and try again")
+	return render(request, 'analyze.html', params)
 
 # def removepunc(request):
 # 	print(request.GET.get('text',"default"))
